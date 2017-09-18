87ccba73fb27dd0de598a389b4d4f2db79296fbe
Sean Kang <neojake911@gmail.com>
2017-09-19 00:04:23 +0900

    add router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import {Input, Menu} from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Router>      
+      <div>      
+        <Header />
+
+        <hr/>
+
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
       </div>
+    </Router>
     );
   }
 }
